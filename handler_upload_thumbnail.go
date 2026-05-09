84192a418ba5ebacc64f964aaf2e374fd50a808a
weilok2021 <weilok629@gmail.com>
2026-05-09 14:09:54 +0800

    Implement thumbnail upload handler with in-memory storage
    Parses multipart form, reads thumbnail bytes, verifies video ownership,
    stores the image in the in-memory videoThumbnails map, and updates the
    video's ThumbnailURL to point at the /api/thumbnails endpoint.

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +29,45 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory int64 = 10 * 1024 * 1024
+	r.ParseMultipartForm(maxMemory)
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse thumbnail form file", err)
+		return
+	}
+	defer file.Close()
+	mediaType := header.Header.Get("Content-Type")
+	imgData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to read image data in file", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	metadata, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to read video meta data from db", err)
+		return
+	}
+
+	// check video ownership
+	if userID != metadata.UserID {
+		respondWithError(w, http.StatusUnauthorized, "Authorization Error: User does not own this video", err)
+	}
+	thumbnail := thumbnail{
+		data:      imgData,
+		mediaType: mediaType,
+	}
+	// Add the thumbnail to the global map, using the video's ID as the key
+	videoThumbnails[videoID] = thumbnail
+	url := fmt.Sprintf("http://localhost:8091/api/thumbnails/%s", videoIDString)
+	metadata.ThumbnailURL = &url
+	err = cfg.db.UpdateVideo(metadata)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to update video's thumbnail", err)
+	}
+	respondWithJSON(w, http.StatusOK, metadata)
 }
